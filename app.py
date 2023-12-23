0ad8011ab29acc0a286b9200919ebe1ddfb90441
Fayyaz Kaleem <fayyazkaleem@outlook.com>
2023-12-23 21:34:55 +0500

    site look nice that is ready to deploy

---
+++ app.py
@@ -0,0 +1,45 @@
+from flask import Flask, render_template, jsonify
+app = Flask(__name__)
+JOBS =[
+    {
+        "id": 1,
+        "position":"Data Analyst",
+        "location":"Lahore Pakistan",
+        "salary":"RS. 1,000,00"
+
+    },
+        {
+        "id": 1,
+        "position":"Python Developer",
+        "location":"Rahim Yar Khan Pakistan",
+        
+
+    },
+    {
+        "id": 1,
+        "position":"Backend Engineer",
+        "location":"Lahore Pakistan",
+        "salary":"RS. 2,000,00"
+
+    },
+    {
+        "id": 4,
+        "position":"Front End Engineer",
+        "location":"Karachi Pakistan",
+        "salary":"RS. 1,000,00"
+
+    }
+]
+
+@app.route("/")
+def hello_world():
+    return render_template('home.html', 
+                           jobs=JOBS, company_name="Fizz")
+
+@app.route("/api/jobs")
+def about():
+    return jsonify(JOBS)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
