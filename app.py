9b9e6b69fae5a596267212dbc2fd998b7873de1d
muhammadfayyazkaleem <muhammadfayyazkaleem@gmail.com>
2023-12-29 13:22:37 +0500

    functional and aesthetic Changes

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ def home():
     return render_template('home.html', 
                            jobs=jobs, company_name="Fizz")
 
+
 @app.route("/api/jobs")
 def about():
     jobs = load_job_from_db()
     return jsonify(jobs)
 
-@app.route("/apply")
-def job_apply():
-    
-    return render_template('apply.html')
 
+@app.route("/apply")
+def job_apply(): 
+    return render_template('apply.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
