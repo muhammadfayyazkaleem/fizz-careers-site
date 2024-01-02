a1aee741fa96cdfda53fed8060bfbaee24a2c93a
muhammadfayyazkaleem <muhammadfayyazkaleem@gmail.com>
2024-01-02 18:01:05 +0500

    make job page dynamic, functional and aesthetic changes and add more templates

--- database.py
+++ database.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine , text
-db_connect_string ="mysql+pymysql://7fzrl8irnvel20dtctqg:pscale_pw_VI1tU4lnrpJCqo6dQhPAWvfeyDH4I7ufNeFLTGVZHg9@aws.connect.psdb.cloud/fizz-careers?charset=utf8mb4"
+db_connect_string ="mysql+pymysql://b48p1gl118azhgzcs681:pscale_pw_FzoVwzsIJDUBBUd0OyYKeMwDjJJUrdkXfrkgyraMs4c@aws.connect.psdb.cloud/fizzcareerdb?charset=utf8mb4"
 engine = create_engine(db_connect_string, connect_args= {
     'ssl': {
         "ssl_ca": "/etc/ssl/cert.pem"
     }
 }) 
 
-def load_job_from_db():
+def load_jobs_from_db():
     with engine.connect() as conn:
         result = conn.execute(text("select * from job"))
         jobs = []
@@ -15,3 +15,12 @@ def load_job_from_db():
         return jobs
 
 
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from job where id = :val"),
+                              val=id)
+        rows = result.fetchall()
+        if len(rows) == 0:
+            return None
+        else:
+            return dict(rows[0])
